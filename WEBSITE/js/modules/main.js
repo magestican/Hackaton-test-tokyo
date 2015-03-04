d47979a9e899f60727135f7ef03aa6668e829f22
magestican <magestican.visualkei@gmail.com>
2015-03-04 23:41:45 +0900

    corrected small syntax error on sass file

--- WEBSITE/js/modules/main.js
+++ WEBSITE/js/modules/main.js
@@ -1,11 +1,10 @@
-require('../lib/angular.js');
+require('../lib/angular.min.js');
 require('../lib/semantic.min.js');
 
 require('./controllers.js');
 require('./directives.js');
 require('./factories.js');
 require('./filters.js');
-require('./services.js');
 
 require('./app.js');
 require('../../templates/compiledhtml.js')
